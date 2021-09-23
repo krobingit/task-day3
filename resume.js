45ec34cb262a6b0fb92c473e4cb4856ac8a763b6
krobingit <91210322+krobingit@users.noreply.github.com>
2021-09-24 01:03:11 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,33 @@
+var personDetails = {
+
+    "name": "Robin",
+    "address": "LIC COLONY,TRICHY",
+    "country": "India",
+    "mobileNo": "9556254569",
+    "emailId": "liorobin5@gmail.com"
+}
+
+var qualificationDetails = {
+
+    "10thSchoolNameAndYearF": "AlphaWisdomSchool2013",
+    "12thSchoolNameAndYearF": "SRVSchool2015",
+    "CollegeAndYearF": "Sastra2019",
+    "CollegeCGPA": "7.05"
+}
+
+var technicalSkills = {
+
+    "skills": "C++,Javascript,HTML,CSS,node.js,react.js"
+}
+
+var personalprofile = {
+    "dob": "01/08/1997",
+    "gender": "male",
+    "languagesKnown": "english,tamil,hindi",
+    "declaration": "i hereby declare that above info is true to my best of knowledge",
+    "sign": "Robin",
+    "place": "trichy",
+    "date": "20/09/2021"
+}
+
+console.log(personDetails, qualificationDetails, technicalSkills, personalprofile)
